1220e37264d8837274e7fe2b12d1a7c6a428542b
Ronald Opara <xspazr@gmail.com>
2019-02-19 13:01:40 -0500

    updated

--- js/script.js
+++ js/script.js
@@ -16,7 +16,38 @@ FSJS project 1 - A Random Quote Generator
       quote object.
 ***/
 
+var quotes = [
+  {
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    source: "Thomas Alva Edison",
+    tags: "Technology"
+  },
+  {
+    quote: "I never left the field saying I could have done more to get ready and that gives me peace of mind.",
+    source: "Peyton Manning",
+    tags: "Sports"
+  },
+  {
+    quote: "The music is not in the notes, but in the silence between.",
+    source: "Wolfgang Amadeus Mozart",
+    tags: "Music"
+  },
+  {
+    quote: "Without commitment, you'll never start, but more importantly, without consistency, you'll never finish.",
+    source: "Denzel Washington",
+    tags: "Entertainment",
+    year: '2017'
+  },
+  {
+    quote: "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.",
+    source: "Steve Jobs",
+    tags: "Technology",
+    year: '2005'
+    
+  },
+]
 
+//  console.log(quotes[0].quote) ;
 
 
 /***
@@ -26,8 +57,17 @@ FSJS project 1 - A Random Quote Generator
      `quotes` array.
 ***/
 
+//This function grabs a random quote from the object located in the array stated above.//
+function getRandomQuote(array) {
 
 
+  var randomNumber = Math.floor( Math.random() * quotes.length) ;
+  randomNumber = quotes[randomNumber]; 
+  
+  return randomNumber;
+}
+
+// // randomNumber = quotes[randomNumber].quote; 
 
 /***
   Create the `printQuote` function to: 
@@ -38,6 +78,28 @@ FSJS project 1 - A Random Quote Generator
      they are added to the HTML string.
    - set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
+
+function printQuote() {
+  var sendQuote = getRandomQuote(); 
+  var html = '<p>' + sendQuote.quote + '</p>'
+  html += '<p>' + sendQuote.source + '</p>';
+  html += '<p>' + sendQuote.tags + '</p>';
+  if ( sendQuote.year || sendQuote ) {
+    html += '<p>' + sendQuote.year + '</p>';
+  } else if (sendQuote.year === "undefined" ) {
+    sendQuote.year += '<p>' + 'N/A' + '</p>';
+  }
+  return html;
+}
+
+
+  
+
+
+// // console.log(prop, ': ', quotes[prop].source);
+
+
+
 
 
 
